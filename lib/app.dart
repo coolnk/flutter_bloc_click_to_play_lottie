7f4105df12a3c4798cc308001b6a02fcd36fd815
coolnk <nirjal@gmail.com>
2022-11-05 21:09:12 +0100

    wip: updated references

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+import 'bloc/lottie_player_bloc.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +14,10 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-        home: BlocProvider());
+        home: BlocProvider<LottiePlayerBloc>(
+          create: (BuildContext context) 
+          },
+        ));
   }
 }
 
