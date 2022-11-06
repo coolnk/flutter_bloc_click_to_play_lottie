b8305641a59034fc229836a25b9f5d0db8570927
coolnk <nirjal@gmail.com>
2022-11-06 19:28:55 +0100

    Added google fonts package

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'bloc/lottie_player_bloc.dart';
 
 class App extends StatelessWidget {
+  final style = const TextStyle(fontSize: 40, fontWeight: FontWeight.bold);
   const App({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,9 +16,10 @@ class App extends StatelessWidget {
           child: Scaffold(
             appBar: AppBar(
               // title: Text(widget.title),
-              title: const Text("Lottie player"),
+              title: Text('Lottie player',
+                  style: GoogleFonts.comfortaa(textStyle: style)),
               centerTitle: true,
-              backgroundColor: Colors.red,
+              backgroundColor: Colors.cyan,
             ),
           )),
     );
