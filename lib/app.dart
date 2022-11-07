90c200bdea1012b63871e05c03cbc9a994069d5b
coolnk <nirjal@gmail.com>
2022-11-07 17:31:14 +0100

    working but there is this error I have to fix with the provider

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_click_to_play_lottie/splash.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 
 import 'bloc/lottie_player_bloc.dart';
-import 'lottie_animation_controller.dart';
 
 class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: SplashScreen());
+  }
+}
+
+class Home extends StatelessWidget {
   final style = const TextStyle(fontSize: 40, fontWeight: FontWeight.bold);
-  const App({Key? key}) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,17 +26,18 @@ class App extends StatelessWidget {
           create: (context) => LottiePlayerBloc(),
           child: Scaffold(
               appBar: AppBar(
-                title: Text('Lottie player',
+                title: Text('Puku kids',
                     style: GoogleFonts.comfortaa(textStyle: style)),
                 centerTitle: true,
-                backgroundColor: Colors.cyan,
+                backgroundColor: Color.fromARGB(255, 14, 77, 61),
               ),
               body: Center(
                 child: BlocConsumer<LottiePlayerBloc, LottiePlayerState>(
                   listener: (context, state) {},
                   builder: (context, state) {
-                    return Text('Coming soon...',
-                        style: GoogleFonts.comfortaa(textStyle: style));
+                    return Lottie.network(
+                      'https://assets7.lottiefiles.com/packages/lf20_etjk2gn2.json',
+                    );
                   },
                 ),
               ),
@@ -35,7 +45,8 @@ class App extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   FloatingActionButton(
-                      child: const Icon(Icons.play_arrow_rounded),
+                      child: const Icon(Icons.play_arrow),
+                      backgroundColor: Color.fromARGB(255, 215, 112, 9),
                       onPressed: () => {
                             context
                                 .read<LottiePlayerBloc>()
